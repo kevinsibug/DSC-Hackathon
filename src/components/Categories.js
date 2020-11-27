2aab3b610c5dbb1927bc2b40e527e907b18826af
Kyla Nicole Villegas <kyla.villegas@obf.ateneo.edu>
2020-11-27 22:39:17 +0800

    started Categories component

---
+++ src/components/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <div className=''>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Categories from '../components/Categories'
 import CTA from '../components/CTA'
 const Home = () => {
     return (
         <div>
             <Header />
             <CTA />
+            <Categories />
         </div>
     )
 }
