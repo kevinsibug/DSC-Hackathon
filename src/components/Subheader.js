7ba32b415e6239ba4ac8d422df7e55ca3d316c19
Kyla Nicole Villegas <kyla.villegas@obf.ateneo.edu>
2020-11-28 12:16:23 +0800

    Added Subheader component

---
+++ src/components/Subheader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Subheader = (props) => {
+    return (
+        <div>
+          <div className='subheader'>
+          <h2>{props.title}</h2>
+          </div>
+          <div className='subheader desc'>
+          {props.desc}
+          </div>
+        </div>
+    )
+}
+
+export default Subheader
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 import Header from '../components/Header'
 import CTA from '../components/CTA'
+import Subheader from '../components/Subheader'
 const Home = () => {
     return (
         <div>
             <Header />
             <CTA />
+            <Subheader title={'Featured Shops'}
+            desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '}/>
+            <Subheader title={'Categories'}
+            desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '}/>
+            <Subheader title={'Partner Shops'}
+            desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '}/>
         </div>
     )
 }
