acb8796e948c8d5fa3edabc1c9f69b6a4e7aa8b6
Kevin Sibug <kevindaniel.sibug@gmail.com>
2020-11-28 19:10:12 +0800

    Removed Shop Content

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -6,15 +6,7 @@ import ListingContext from "../context/listingContext";
 
 import Header from "../components/Header"
 const Shop = (props) => {
-  const { firestore } = props;
-  const listingContext = useContext(ListingContext);
 
-  const { shop } = listingContext;
-
-  const shopRef = firestore.collection("shops").where("name", "==", `${shop}`);
-
-  const [shopSelected] = useCollectionData(shopRef, { idField: "id" });
-  console.log(shopSelected);
   return (
     <div>
       <Header />
